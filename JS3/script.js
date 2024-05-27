ece3a1ae75e78f942ef74a7a513b5cd59bef4b5f
Jose Getulio <getuliojose96@gmail.com>
2024-05-26 21:45:39 -0400

    Atividade 3 de JS

---
+++ JS3/script.js
@@ -0,0 +1,63 @@
+class IntergerSet{
+
+    
+
+
+    constructor (max)
+    {
+
+        this.max = max
+        this.vetor = [false, false, false]
+
+    }
+
+    insercao()
+    {
+        if (element >= 0 && element <= this.max) 
+        {
+            this.vetor[element] = true;
+        }
+    }
+
+    exclusao()
+    {
+
+    }
+
+    uniao()
+    {
+
+    }
+
+    intersecao()
+    {
+
+    }
+
+    diferenca()
+    {
+
+    }
+
+    toString()
+    {
+
+    }    
+
+}
+
+
+let conj1 = IntergerSet(20)
+let conj2 = IntergerSet(50)
+let conj3 = IntergerSet(100)
+
+let conjuntos = [conj1, conj2, conj3];
+
+for(let i = 0; i < 10; i++)
+{
+    console.log("Insira um numero ")
+    let num = parseInt(prompt())
+    
+
+}
+
