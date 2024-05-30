e89c62d727260935d2aa451359430ebd7f755cad
Jose Getulio <getuliojose96@gmail.com>
2024-05-30 19:44:22 -0400

    Melhoria no exercicio 4 de JS

--- JS4/script.js
+++ JS4/script.js
@@ -1,5 +1,12 @@
-let raio = document.formulario.raio.value
+document.formulario.submit.onclick = function(){
 
-document.formulario.area.value = 3.14 * raio * raio
 
-document.formulario.circunferencia.value = 3.14 * 2 * raio
+    event.preventDefault();
+
+    let raio = document.formulario.raio.value
+
+    document.formulario.area.value = 3.14 * raio * raio
+
+    document.formulario.circunferencia.value = 3.14 * 2 * raio
+
+}
