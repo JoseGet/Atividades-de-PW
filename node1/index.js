6d388234486c23c0000c14e654fd7f187fe7ab8a
Jose Getulio <getuliojose96@gmail.com>
2024-06-30 13:04:03 -0400

    Exercicio de node.js parte 1

---
+++ node1/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+let diretorio = ""
+
+process.argv.forEach((val) => {
+    diretorio = val
+})
+
+
+const http = require('http');
+const server = http.createServer(function(req,res){
+res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+
+    fs.readdir(diretorio, (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            files.forEach(file => {
+                res.write(file + "<br>");
+            })
+        }
+        res.end();
+    })
+});
+server.listen(3333);
+
+
