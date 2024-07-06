e13c2b30bfcda1c6338c410c54b41385575ce391
Jose Getulio <getuliojose96@gmail.com>
2024-07-06 14:17:51 -0400

    Exercico 1 de node.js parte 6

--- node1/index.js
+++ node1/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const dotenv = require("dotenv")
 
 let diretorio = ""
+const strHelper = require('./str_helper')
 
 process.argv.forEach((val) => {
     diretorio = val
@@ -9,21 +10,36 @@ process.argv.forEach((val) => {
 
 
 const http = require('http');
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 const PORT = process.env.PORT ?? 8080;
-const server = http.createServer(function(req,res){
-res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+const server = http.createServer(function (req, res) {
+res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
 
-    fs.readdir(diretorio, (err, files) => {
-        if (err) {
-            console.log(err);
-        } else {
-            files.forEach(file => {
-                res.write(file + "<br>");
-            })
-        }
-        res.end();
-    })
+    if (req.url === "/") {
+        fs.readdir(diretorio, (err, files) => {
+            if (err) {
+                console.log(err);
+            } else {
+                files.forEach(file => {
+                    res.write(strHelper.createLink(file) + "<br>");
+                });
+                res.end();
+            }
+        });
+    } else {
+        const filePath = diretorio + req.url;
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const texto = "Ola! Voce entrou no arquivo, clique em Voltar para sair :)";
+                const textoModificado = texto + data;
+                res.write(`<a href="/">Voltar</a><br><br>`);
+                res.write(`<pre>${textoModificado}</pre>`);
+                res.end();
+            }
+        });
+    }
 });
 server.listen(PORT);
 
