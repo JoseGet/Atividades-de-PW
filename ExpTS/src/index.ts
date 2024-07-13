724f764dd120555dea4d4bf1acc3b9d476a32e23
Jose Getulio <getuliojose96@gmail.com>
2024-07-13 15:09:52 -0400

    Exercicio 3 de Express

--- ExpTS/src/index.ts
+++ ExpTS/src/index.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import morgan from 'morgan';
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3333;
+app.use(morgan('short'));
+
 app.get("/", (req: Request, res: Response) => {
-res.send("Hello world!");
+
+    res.send("Hello world!");
+
 });
+
 app.listen(PORT, () => {
-console.log(`Express app iniciada na porta ${PORT}.`);
+
+    console.log(`Express app iniciada na porta ${PORT}.`);
+    
 });
